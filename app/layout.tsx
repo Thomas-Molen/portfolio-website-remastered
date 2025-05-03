85bf94380e0a06b19956b838259859b27b076ee8
Thomas-Molen <thomasmolen3@gmail.com>
2025-05-03 16:40:07 +0200

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from 'next/font/google'
 import "./globals.css";
 
@@ -7,7 +7,22 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: "Thomas Van der Molen",
   description: "Portfolio website of Thomas Van der Molen",
+  icons: {
+    icon: [
+      { url: "/meta/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/meta/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    apple: "/meta/apple-touch-icon.png",
+    other: [
+      { rel: "mask-icon", url: "meta/safari-pinned-tab.svg", color: "#333333" },
+    ]
+  },
+  manifest: "/meta/site.webmanifest"
 };
+
+export const viewport: Viewport = {
+  themeColor: "#09090b",
+}
 
 export default function RootLayout({
   children,
