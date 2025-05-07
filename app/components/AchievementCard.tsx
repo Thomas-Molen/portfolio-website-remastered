bc62c63e70ab111c375896cb07021eed4b694a8d
Thomas-Molen <thomasmolen3@gmail.com>
2025-05-07 21:21:44 +0200

    Add appear animation to achievement card

--- app/components/AchievementCard.tsx
+++ app/components/AchievementCard.tsx
@@ -1,15 +1,18 @@
 import React from "react";
+import styles from './AchievementCard.module.css';
 
 interface AchievementCardProps {
     title: string;
     body: string;
     description: string;
     icon?: React.ReactNode;
+    appearDelay?: string;
 }
 
-export default function AchievementCard({title, body, description, icon}: AchievementCardProps) {
+export default function AchievementCard({title, body, description, icon, appearDelay = "0s"}: AchievementCardProps) {
     return (
-        <div className="flex flex-auto flex-col h-full justify-around bg-background border border-muted rounded-md p-6 font-medium">
+        <div className={`flex flex-auto flex-col h-full justify-around bg-background border border-muted rounded-md p-6 font-medium ${styles.slideUp}`}
+            style={{ animationDelay: appearDelay }}>
             <div className="mb-2 flex flex-row items-center justify-between">
                 <p className="text-sm">{title}</p>
                 {icon}
