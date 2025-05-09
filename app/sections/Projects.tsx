76ccb50f07b64910eb7f2a5dff7a53bc2e513ac6
Thomas-Molen <thomasmolen3@gmail.com>
2025-05-09 10:55:05 +0200

    Move preferedProject out of useEffect

--- app/sections/Projects.tsx
+++ app/sections/Projects.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Suspense, useEffect, useState } from "react";
+import { Suspense, useState } from "react";
 import ProjectCard, { SkeletonProjectCard } from "../components/ProjectCard"
 import { ProjectTimeline, SkeletonProjectTimeline } from "../components/ProjectTimeline";
 import _projects from "@/projects.json"
@@ -8,60 +8,55 @@ import type Project from "@/types/project"
 import { useSearchParams, useRouter } from "next/navigation";
 
 export default function Projects() {
-    const [selectedProject, setSelectedProject] = useState<Project | undefined>(undefined);
+  function TimelineView() {
+    const queryParams = useSearchParams();
+    const router = useRouter();
 
-    function TimelineView() {
-        const queryParams = useSearchParams();
-        const router = useRouter();
+    // Determine initial selected project from query param
+    const queryProject = queryParams.get("project");
+    const preferedProject = _projects.find(p => p.project === queryProject) ?? _projects[0];
+    const [selectedProject, setSelectedProject] = useState<Project>(preferedProject);
 
-        useEffect(() => {
-            if (selectedProject) return;
+    function SelectProject(project: Project) {
+      setSelectedProject(project);
 
-            const queryProject = queryParams.get("project");
-            const preferredProject = _projects.find(p => p.project === queryProject);
+      const fragmentIdentifier = window.location.hash;
+      const currentSearchParams = new URLSearchParams(queryParams as URLSearchParams);
+      currentSearchParams.set("project", project.project);
 
-            setSelectedProject(preferredProject ?? _projects[0]);
-        }, [queryParams, selectedProject])
-
-        function SelectProject(project: Project) {
-            setSelectedProject(project);
-
-            const fragmentIdentifier = window.location.hash; // Explicitely store fragment identifier as next navigation will void it
-
-            const currentSearchParams = new URLSearchParams(queryParams as URLSearchParams);
-            currentSearchParams.set("project", project.project);
-            router.replace(`${window.location.pathname}?${currentSearchParams.toString()}${fragmentIdentifier}`, { scroll: false });
-        }
-
-        return (
-            <div className="hidden md:grid md:grid-cols-8 xl:grid-cols-10 gap-x-8" >
-                <div className="col-span-4 h-[470px] lg:h-[400px]">
-                    {selectedProject ?
-                        <ProjectCard project={selectedProject} />
-                        :
-                        <SkeletonProjectCard />
-                    }
-                </div>
-                <div className="col-span-4">
-                    {selectedProject ?
-                        <ProjectTimeline
-                            projects={_projects}
-                            onSelect={(selectedProject => SelectProject(selectedProject))}
-                            selectedProject={selectedProject} />
-                        :
-                        <SkeletonProjectTimeline />
-                    }
-                </div>
-            </div>
-        )
+      router.replace(`${window.location.pathname}?${currentSearchParams.toString()}${fragmentIdentifier}`, { scroll: false });
     }
 
     return (
-        <>
-            <h2 className="text-3xl font-bold text-center mb-16">Projects Portfolio</h2>
-            <Suspense>
-                <TimelineView />
-            </Suspense>
-        </>
-    )
+      <div className="hidden md:grid md:grid-cols-8 xl:grid-cols-10 gap-x-8">
+        <div className="col-span-4 h-[470px] lg:h-[400px]">
+          {selectedProject ? (
+            <ProjectCard project={selectedProject} />
+          ) : (
+            <SkeletonProjectCard />
+          )}
+        </div>
+        <div className="col-span-4">
+          {selectedProject ? (
+            <ProjectTimeline
+              projects={_projects}
+              onSelect={SelectProject}
+              selectedProject={selectedProject}
+            />
+          ) : (
+            <SkeletonProjectTimeline />
+          )}
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <h2 className="text-3xl font-bold text-center mb-16">Projects Portfolio</h2>
+      <Suspense>
+        <TimelineView />
+      </Suspense>
+    </>
+  );
 }
