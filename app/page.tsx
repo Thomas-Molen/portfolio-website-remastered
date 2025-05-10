3bc986fd0840e78bedf1e4015c48bd253177aac8
Thomas Molen <sugarypineapple2@gmail.com>
2025-05-11 00:34:08 +0200

    Add contact section and footer

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "./sections/Hero"
 import Companies from "./sections/Companies"
 import AboutMe from "./sections/AboutMe"
 import Projects from "./sections/Projects"
+import Contact from "./sections/Contact"
 
 export default function Page() {
   return (
@@ -28,8 +29,13 @@ export default function Page() {
           <Projects />
         </div>
       </section>
-      <section id="contact">
-
+      <section id="contact" className="bg-background-muted py-24">
+        <Contact />
+      </section>
+      <section id="footer" className="text-foreground-muted text-sm">
+        <div className="container xl:max-w-7xl p-5 mx-auto">
+          Open source project by <a href="https://github.com/Thomas-Molen/portfolio-website-remastered" target="_blank" className="text-primary">Thomas Van der Molen</a>.
+        </div>
       </section>
     </div>
   )
