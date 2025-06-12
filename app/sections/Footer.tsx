416ed9a49972db40d414b5f254a6a705c0d3251a
Thomas-Molen <thomasmolen3@gmail.com>
2025-06-12 19:31:46 +0200

    Change open source link to be full text

--- app/sections/Footer.tsx
+++ app/sections/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <div className="container py-5 text-center sm:flex sm:justify-between text-foreground-muted text-sm">
             <p>
-                Open source project by <a href="https://github.com/Thomas-Molen/portfolio-website-remastered" target="_blank" className="text-primary">Thomas Van der Molen</a>.
+                <a href="https://github.com/Thomas-Molen/portfolio-website-remastered" target="_blank">Open source project by <span className="text-primary">Thomas Van der Molen</span></a>
             </p>
             <a className="underline" href="https://github.com/Thomas-Molen/portfolio-website-remastered/blob/4fe9544510c2a079e164c5d462c76ed3d4c4dc41/LICENSE" target="_blank">MIT License</a>
         </div>
